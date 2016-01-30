0828e433331249e4092bf763230d9ab1986b071a
Luan Santos <luankjs@gmail.com>
2016-01-30 14:44:14 -0300

    adjust n:n association between student and courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ActiveRecord::Base
+  has_many :classrooms
+  has_many :students, through: :classrooms
+
 	validates :name, presence: true
 	validates :description, presence: true
 	validates :status, presence: true
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ActiveRecord::Base
+  has_many :classrooms
+  has_many :courses, through: :classrooms
+
 	validates :name, presence: true
 	validates :register_number, presence: true, uniqueness: true
 	validates :status, presence: true
