6d967e5c7a6667046cdd3b8b9ef9788cebc65754
Luan Santos <luankjs@gmail.com>
2016-01-30 00:26:34 -0300

    implementing validates to Student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,15 @@
 class Student < ActiveRecord::Base
+	validates :name, presence: true
+	validates :register_number, presence: true, uniqueness: true
+	validates :status, presence: true
+	validates_inclusion_of :status, :in => [0,1]
+
+  STUDENT_STATUSES = {
+  	0 => :inactive
+    1 => :active
+  }
+
+  def status_str
+    I18n.t(STUDENT_STATUSES[self.status], scope: "student_statuses")
+  end
 end
