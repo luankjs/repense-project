3c548d7274208788cb54d67cda0f9de6786a1d5a
Luan Santos <luankjs@gmail.com>
2016-01-30 21:43:55 -0300

    implements bootstrap theme

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :courses
   resources :students
 
+  root 'students#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
