1276eb4cddcfc32ac34621ac750f60c9f5d65b29
Luan Santos <luankjs@gmail.com>
2016-01-30 21:10:20 -0300

    validate if student already registered on selected course

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -5,9 +5,17 @@ class Classroom < ActiveRecord::Base
   validates :student, presence: true
   validates :course, presence: true
 
+  validate :student_already_registered
+
   before_create :set_entry_at
 
   def set_entry_at
   	self.entry_at = Time.zone.now  	
+  end
+
+  def student_already_registered
+  	if Classroom.find_by(student: self.student, course: self.course)
+  		errors.add(:course_id, I18n.t(:already_registered, scope: "activerecord.errors.models.classroom.attributes.course"))
+  	end
   end
 end
